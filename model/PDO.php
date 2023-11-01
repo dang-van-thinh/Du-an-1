f0cec59bdb2aa579d95df0410bd3f55fa8dfe157
Dang Van Thinh <dangvanthinh372004@gmail.com>
2023-11-01 15:29:50 +0700

    update template admin and client

--- model/PDO.php
+++ model/PDO.php
@@ -1,4 +1,23 @@
 <?php
 require_once 'ConnectDB.php';
-
+function pdo_query($sql){
+    $conn = connect();
+    $stmt = $conn->prepare($sql);
+    return $stmt->fetchAll();
+}
+function pdo_excute($sql){
+    $conn = connect();
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+}
+function pdo_query_one($sql){
+    $conn = connect();
+    $stmt = $conn->prepare($sql);
+    return $stmt->fetch();
+}
+function pdo_query_value($sql){
+    $conn = connect();
+    $stmt = $conn->prepare($sql);
+    return $stmt->fetch()[0];
+}
 ?>
---
+++ view/assets/js/admin.js
@@ -0,0 +1,47 @@
+let check_all = document.querySelector('#checkAll')
+
+check_all.addEventListener('click', function (param) {
+    let check_box = document.querySelectorAll('.item_checkbox');
+
+    if (check_all.checked == true) {
+        for (let i = 0; i < check_box.length; i++) {
+            check_box[i].checked = true
+        }
+    } else {
+        for (let i = 0; i < check_box.length; i++) {
+            check_box[i].checked = false
+        }
+    }
+})
+// chọn tất cả
+let selectAll = document.querySelector('#selectAll');
+selectAll.addEventListener('click', function (param) {
+    let check_box = document.querySelectorAll('.item_checkbox');
+    for (let i = 0; i < check_box.length; i++) {
+        check_box[i].checked = true
+    }
+
+});
+// bỏ chọn tất cả
+let unSelectAll = document.querySelector('#unSelectAll');
+unSelectAll.addEventListener('click', function (param) {
+    let check_box = document.querySelectorAll('.item_checkbox');
+    for (let i = 0; i < check_box.length; i++) {
+        check_box[i].checked = false
+    }
+
+});
+
+// xóa tât cả theo lựa chọn
+
+let delSelect = document.querySelector('#delSelect');
+delSelect.addEventListener('click',function (param) { 
+    let item_checked = document.querySelectorAll('input[name=check_sp]:checked');
+    for (let i = 0; i < item_checked.length; i++) {
+        if(item_checked[i]){
+            console.log(item_checked[i].parentNode);
+            // chưa xong
+        }
+    }
+ })
+
