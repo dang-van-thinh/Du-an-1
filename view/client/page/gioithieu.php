2431fdb133790435aded4180f92dc52826d21d26
hoalvph <128578103+hoalvph@users.noreply.github.com>
2023-11-24 07:45:34 +0700

    test

--- view/client/page/gioithieu.php
+++ view/client/page/gioithieu.php
@@ -5,3 +5,32 @@
     <div class="item"><img src="../view/assets/img/banner3.avif" alt="" /></div>
     <div class="item"><img src="../view/assets/img/banner2.avif" alt="" /></div>
 </div>
+<div class="container">
+    <div class="row">
+    <div class="col-md-9 col-sm-12 col-xs-12">
+            <div class="page-wrapper">
+              <div class="heading-page">
+                <h1>Giới thiệu</h1>
+              </div>
+              <div class="wrapbox-content-page">
+                <div class="content-page ">
+                 <p><h3><span>SPORTDA1 - WEBSITE MUA SẮM THỜI TRANG  VỀ THỂ THAO  TRỰC TUYẾN VIỆT NAM</span></h3></p>
+                 <p>BẠN ĐANG TÌM KIẾM NHỮNG MẪU THỜI TRANG, QUẦN ÁO VÀ PHỤ KIỆN MỚI NHẤT TRÊN MẠNG? HÃY MUA SẮM NGAY HÔM NAY TẠI SPORTDA1!</p>
+                 <p>SPORTDA1 sẽ mang lại cho khách hàng những trải nghiệm mua sắm thời trang về thể thao trực tuyến thú vị từ các thương hiệu thời trang quốc tế và trong nước,
+                   cam kết chất lượng phục vụ hàng đầu cùng với những bộ sưu tập quần áo nam nữ khổng lồ từ trang phục, phụ kiện cho phụ nữ với những xu hướng thời trang mới nhất.
+                   Bạn có thể tìm thấy những bộ trang phục mình muốn từ những bộ đồ ở nhà thật thoải mái hay tự tin trong những bộ trang phục thể thao phù hợp. 
+                   Những trải nghiệm mới chỉ có ở trang mua sắm hàng hiệu trực tuyến SPORTDA1.</p>
+                   <p><h3><span>MUA SẮM Ở SPORTDA1 - PHÙ HỢP VỚI TÚI TIỀN TỪ  NHÂN VIÊN VĂN PHÒNG ĐẾN CÁC BẠN TRẺ </span></h3></p>
+                   <p>SPORTDA1 luôn cập nhật những xu hướng thời trang phong cách nhất đều có ở SPORTDA1.
+                      Đặc biệt, SPORTDA1 còn có nhiều đợt khuyến mãi, giảm giá đặc biệt với giá tốt nhất. Thời trang hàng hiệu chất lượng cao, phù hợp túi tiền - chỉ có ở SPORTDA1!</p>
+                  <p><h3><span>PHONG CÁCH MUA SẮM HIỆN ĐẠI - THUẬN TIỆN, THANH TOÁN AN TOÀN - DỄ DÀNG</span></h3></p>
+                  <p>Bạn có thể mua sắm thoải mái trên SPORTDA1 mà không có bất kỳ lo lắng nào: trả hàng trong vòng 30 ngày kể từ ngày nhận hàng.
+                    Nếu bạn có bất kì câu hỏi nào về các sản phẩm của chúng tôi từ trang phục nữ hãy gọi ngay tới bộ phận chăm sóc khách hàng.</p>
+                  
+                 
+                </div>
+              </div>
+            </div>
+          </div>
+    </div>
+</div>
